497edd3f4bfdc9ae96ac035a61b2ea9feeed9637
Newton Gonzaga Costa <ncosta.rj@gmail.com>
2018-06-10 01:15:20 -0300

    exemplo multi pagina

--- robot-bs4.py
+++ robot-bs4.py
@@ -1,29 +1,281 @@
+import requests
 from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+import time
+from pymongo import MongoClient
 
 class Robot():
 
-    html = ''
+    urls = []
+    posts = []
+    client = MongoClient('localhost', 27017)
+    db = client.symfony_blog
+    collection = db.posts
 
-    def __init__(self, page):
-        self.html = BeautifulSoup(page, 'html.parser')      
-    def extract_head_title(self):
-        return self.html.head.title.get_text().strip()
+    def __init__(self, urls):
+         self.urls = urls
+   
+    def extract_posts(self, html):
+
+        div_posts = html.find_all('div', class_="post__excerpt")
+
+        for post in div_posts:
+            title = post.h2.a
+            headline = post.h2.next_sibling.next_sibling
+            date = headline.next_sibling.next_sibling.span
+            strDate = date.get_text('', strip=True)
+            fDate = datetime.strptime(strDate, '%B %d, %Y').strftime("%Y-%m-%dT%H:%M:%S.000Z+0000")
+            author = date.next_sibling.next_sibling
+            p = dict({'title': title.get_text('', strip=True),'headline': headline.get_text('', strip=True), 'date': fDate,'author': author.get_text('',strip=True)})
+            self.collection.insert(p)
+            self.posts.append(p)
+    
+    def crawl_url(self):
+        for url in self.urls:
+            response = requests.get(url, verify=False)
+            html = BeautifulSoup(response.text, 'html.parser')
+            self.extract_posts(html)
+        # print(json.dumps(self.posts))
+        # self.html =
+    
+    def crawl_html(self, html_doc):
+        html = BeautifulSoup(html_doc, 'html.parser')
+        self.extract_posts(html)
 
 
 html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
+<div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/symfonycon-lisbon-only-100-early-bird-tickets-available">
+                    SymfonyCon Lisbon: only 100 early bird tickets available!
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        We're so happy to announce SymfonyCon Lisbon 2018! We've just released the official website, the international Symfony conference  will be held at the Lisbon Mariott Hotel on December 6-8!
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    June 4, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/71e2f393-158c-4a1c-92c4-2234548e09f5">
+<img alt="Avatar of Emilie Lorenzo" class="avatar" src="https://connect.sensiolabs.com/api/images/71e2f393-158c-4a1c-92c4-2234548e09f5.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/71e2f393-158c-4a1c-92c4-2234548e09f5">
+            Emilie Lorenzo
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/a-week-of-symfony-596-28-may-3-june-2018">
+                    A week of symfony #596 (28 May - 3 June 2018)
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        This week, <a href="https://symfony.com/blog/symfony-4-1-0-released">Symfony 4.1.0 was released</a>, which includes more than 200 big and small <a href="https://symfony.com/blog/symfony-4-1-curated-new-features">new features</a>. In addition, the <a href="https://lisbon2018.symfony.com/registration/">registration</a> for the SymfonyCon Lisbon 2018 conference opened with the first 100 early bird tickets available.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    June 3, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+<img alt="Avatar of Javier Eguiluz" class="avatar" src="https://connect.sensiolabs.com/api/images/17170f22-52c0-4adf-a75d-29cb01d53176.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+            Javier Eguiluz
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/symfony-4-1-0-released">
+                    Symfony 4.1.0 released
+                </a>
+</h2>
+<p class="m-b-5">
+<a href="/blog/symfony-4-1-0-released">
+                        Read release notes
+                    </a>
+</p>
+<p class="metadata">
+<span class="m-r-15">
+    May 30, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+<img alt="Avatar of Fabien Potencier" class="avatar" src="https://connect.sensiolabs.com/api/images/4aed4f5d-e0cb-4320-902f-885fddaa7d15.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+            Fabien Potencier
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/symfony-4-1-curated-new-features">
+                    Symfony 4.1 curated new features
+                </a>
+</h2>
+<p class="m-b-5">
+<a href="/blog/symfony-4-1-curated-new-features">
+                        Read release notes
+                    </a>
+</p>
+<p class="metadata">
+<span class="m-r-15">
+    May 30, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+<img alt="Avatar of Fabien Potencier" class="avatar" src="https://connect.sensiolabs.com/api/images/4aed4f5d-e0cb-4320-902f-885fddaa7d15.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+            Fabien Potencier
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/new-in-symfony-4-1-misc-improvements-part-4">
+                    New in Symfony 4.1: Misc. improvements (part 4)
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        In Symfony 4.1, AbstractController provides a getParameter() helper, anonymous services can be configured with PHP DSL, PropertyInfo can introspect information using the constructor arguments and the level of the PHP logger is configurable.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    May 29, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+<img alt="Avatar of Javier Eguiluz" class="avatar" src="https://connect.sensiolabs.com/api/images/17170f22-52c0-4adf-a75d-29cb01d53176.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+            Javier Eguiluz
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/new-in-symfony-4-1-misc-improvements-part-3">
+                    New in Symfony 4.1: Misc. improvements (part 3)
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        In Symfony 4.1, MoneyType rounding is configurable, updating LDAP entries is more efficient, query strings can be kept when redirecting and hassers are supported by the PropertyInfo component.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    May 28, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+<img alt="Avatar of Javier Eguiluz" class="avatar" src="https://connect.sensiolabs.com/api/images/17170f22-52c0-4adf-a75d-29cb01d53176.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+            Javier Eguiluz
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/a-week-of-symfony-595-21-27-may-2018">
+                    A week of symfony #595 (21-27 May 2018)
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        This week Symfony released <a href="https://symfony.com/blog/symfony-2-7-48-released">2.7.48</a>, <a href="https://symfony.com/blog/symfony-2-8-41-released">2.8.41</a>, <a href="https://symfony.com/blog/symfony-3-3-17-released">3.3.17</a>, <a href="https://symfony.com/blog/symfony-3-4-11-released">3.4.11</a> and <a href="https://symfony.com/blog/symfony-4-0-11-released">4.0.11</a> versions to address several security vulnerabilities. Meanwhile <a href="https://symfony.com/blog/symfony-4-1-0-beta3-released">Symfony 4.1.0 beta3</a> was published in preparation for next week's final release. Lastly, <a href="https://twitter.com/symfony_live/status/999660172502020096">it was announced</a> that the SymfonyLive USA 2018 conference will take place in San Francisco on October 11th and 12th.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    May 27, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+<img alt="Avatar of Javier Eguiluz" class="avatar" src="https://connect.sensiolabs.com/api/images/17170f22-52c0-4adf-a75d-29cb01d53176.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/17170f22-52c0-4adf-a75d-29cb01d53176">
+            Javier Eguiluz
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/symfony-4-1-0-beta3-released">
+                    Symfony 4.1.0-BETA3 released
+                </a>
+</h2>
+<p class="m-b-5">
+<a href="/blog/symfony-4-1-0-beta3-released">
+                        Read release notes
+                    </a>
+</p>
+<p class="metadata">
+<span class="m-r-15">
+    May 26, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+<img alt="Avatar of Fabien Potencier" class="avatar" src="https://connect.sensiolabs.com/api/images/4aed4f5d-e0cb-4320-902f-885fddaa7d15.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+            Fabien Potencier
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/cve-2018-11407-unauthorized-access-on-a-misconfigured-ldap-server-when-using-an-empty-password">
+                    CVE-2018-11407: Unauthorized access on a misconfigured LDAP server when using an empty password
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        CVE-2018-11407 fixes an unauthorized access on a misconfigured LDAP server when using an empty password.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    May 25, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+<img alt="Avatar of Fabien Potencier" class="avatar" src="https://connect.sensiolabs.com/api/images/4aed4f5d-e0cb-4320-902f-885fddaa7d15.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+            Fabien Potencier
+        </a>
+</span>
+</p>
+</div>, <div class="post__excerpt">
+<h2 class="m-b-5">
+<a href="/blog/cve-2018-11385-session-fixation-issue-for-guard-authentication">
+                    CVE-2018-11385: Session Fixation Issue for Guard Authentication
+                </a>
+</h2>
+<p class="m-b-5">
+                                                                        CVE-2018-11385 fixes a session fixation issue when using Guard authentication.
+                            </p>
+<p class="metadata">
+<span class="m-r-15">
+    May 25, 2018
+</span>
+<span>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+<img alt="Avatar of Fabien Potencier" class="avatar" src="https://connect.sensiolabs.com/api/images/4aed4f5d-e0cb-4320-902f-885fddaa7d15.png?format=80x80"/>
+</a>
+<a href="https://connect.sensiolabs.com/api/alternates/4aed4f5d-e0cb-4320-902f-885fddaa7d15">
+            Fabien Potencier
+        </a>
+</span>
+</p>
+</div>
 """
+# urls = ['https://symfony.com/blog/']
+urls = ['https://symfony.com/blog/','https://symfony.com/blog/?page=2','https://symfony.com/blog/?page=3','https://symfony.com/blog/?page=4']
 
-robot = Robot(html_doc)
-print(robot.extract_head_title())
+robot = Robot(urls)
+robot.crawl_url()
 
